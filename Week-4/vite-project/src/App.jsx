c3cd7fbbae1756bf64b192f89ff4aae77cdc995c
noumaan <noumaanahamed2@gmail.com>
2023-07-02 17:20:23 +0530

    week 4 day 1

--- Week-4/vite-project/src/App.jsx
+++ Week-4/vite-project/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoForToday, setTodoForToday] = React.useState({
+    title: "Go to gym",
+    description: "Hit gym from 7-9",
+    id: 1,
+  });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  setInterval(() => {
+    setTodoForToday({
+      title: "Go to gym please please please" + Math.random(),
+      description: "Hit gym from 7-9",
+      id: 2,
+    });
+  }, 10000);
+
+  console.log(todoForToday.title);
+
+  return <RenderTodo todoForToday={todoForToday}></RenderTodo>;
 }
 
-export default App
+function RenderTodo(props) {
+  console.log(props);
+  return (
+    <div id="mainArea">
+      {props.todoForToday.title}
+      <br />
+      {props.todoForToday.description}
+    </div>
+  );
+}
+
+// let todos = [
+//   {
+//     title: "Gym",
+//     description: "Go to gym",
+//     id: 1,
+//   },
+//   {
+//     title: "Eat",
+//     description: "Go to eat",
+//     id: 2,
+//   },
+// ];
+
+// function App() {
+//   const [todo, setTodo] = useState({
+//     title: "Gym",
+//     description: "Go to gym",
+//     id: 1,
+//   });
+
+//   setInterval(() => {
+//     setTodo({
+//       title: "Eat",
+//       description: "Go to eat",
+//       id: 2,
+//     });
+//   }, 2000);
+
+//   return (
+//     <>
+//       <h1>Hello There</h1>
+//       <div>{todo.title}</div>
+//       <div>{todo.description}</div>
+//       <div>{todo.id}</div>
+//       <PersonName firstName={todo.title} middleName={"Noumaan"}></PersonName>
+//     </>
+//   );
+// }
+
+// function PersonName(props) {
+//   return (
+//     <div>
+//       {props.firstName} {props.middleName}{" "}
+//     </div>
+//   );
+// }
+
+export default App;
